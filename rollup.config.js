577f0042a685c098f5e4ea1f2d87f9aba0f3a6a1
Nico Rehwaldt <git_nikku@nixis.de>
2022-03-24 21:35:16 +0100

    feat: provide pre-built distribution
    Also generate CSS.
    
    Closes #1

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import css from 'rollup-plugin-css-only';
+
+import pkg from './package.json';
+
+function pgl(plugins=[]) {
+  return [
+    css({ output: 'element-template-chooser.css' }),
+    resolve({
+      mainFields: [
+        'browser',
+        'module',
+        'main'
+      ]
+    }),
+    commonjs(),
+    ...plugins
+  ];
+}
+
+const deps = [
+  ...Object.keys(pkg.peerDependencies || {}),
+  ...Object.keys(pkg.dependencies || {})
+];
+
+const external = (id) => {
+  return deps.some(dep => id.startsWith(dep) || id === dep);
+};
+
+const srcEntry = pkg.source;
+
+export default [
+  {
+    input: srcEntry,
+    output: [
+      { file: pkg.main, format: 'cjs', exports: 'auto', sourcemap: true },
+      { file: pkg.module, format: 'es', sourcemap: true }
+    ],
+    external: external,
+    plugins: pgl()
+  }
+];
